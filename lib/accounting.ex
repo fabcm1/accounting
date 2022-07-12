7cd2d5a10ef9afc9e2e9de24099dc56cc7ecef29
Fabricio <fabcm1@gmail.com>
2022-07-12 16:44:21 -0300

    Implement process in parallel with streams

--- lib/accounting.ex
+++ lib/accounting.ex
@@ -14,6 +14,12 @@ defmodule Accounting do
 
   """
   def get_value(invoice) do
+  #  NEXT: 
+  #  - make vNF a default parameter, perhaps use get_value(invoice, fields \\ ["vNF"])
+  #  - handle errors     
+  
+  # IO.puts(:stderr, "message")
+  
     {:ok, fileasstring} = File.read(invoice)
 
     Regex.run(~r'<vNF>\d+.\d*</vNF>', fileasstring)
@@ -21,5 +27,16 @@ defmodule Accounting do
       |> String.replace(["<vNF>", "</vNF>"], "") 
       |> String.to_float
   end
-#  NEXT: make vNF a default parameter
+  
+  def process_sequential() do
+    Path.wildcard("invoices/*.xml")
+      |> Enum.reduce(0.0, fn (invoice, acc) -> acc + get_value(invoice) end)
+  end
+  
+  def process_parallel() do
+    Path.wildcard("invoices/*.xml")
+      |> Task.async_stream(&get_value/1)
+      |> Enum.reduce(0.0, fn ({:ok, value}, acc) -> value + acc end)
+  end
+
 end
