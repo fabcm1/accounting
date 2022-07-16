4e32b2c7a74ff440f016550bfe6d6680db9833b4
Fabricio <fabcm1@gmail.com>
2022-07-15 22:55:04 -0300

    change the data type for currency from float to decimal

--- lib/accounting.ex
+++ lib/accounting.ex
@@ -9,7 +9,7 @@ defmodule Accounting do
 
   def handle_error(invoice) do
     IO.puts(:stderr, "#{invoice} does not have a valid vNF field")
-    0.0
+    Decimal.new(0)
   end
 
   @doc """
@@ -24,36 +24,33 @@ defmodule Accounting do
   def get_value(invoice) do
   #  NEXT: 
   #  - make vNF a default parameter, perhaps use get_value(invoice, fields \\ ["vNF"])  
+  #  - replace Decimal with Money
+  #  - try to find the contents in the fileasstring without using regex
+  #  - get the emission date of the invoice
   
     {:ok, fileasstring} = File.read(invoice)
-
-#    Regex.run(~r'<vNF>\d+.\d*</vNF>', fileasstring)
-#      |> hd
-#      |> String.replace(["<vNF>", "</vNF>"], "") 
-#      |> String.to_float
       
-    case Regex.run(~r'<vNF>\d+.\d*</vNF>', fileasstring) do
+    case Regex.run(~r'<vNF>\d+\.\d\d</vNF>', fileasstring) do
       nil -> handle_error(invoice)
-      [pattern] -> 
-        try do 
-          pattern
-            |> String.replace(["<vNF>", "</vNF>"], "") 
-            |> String.to_float
-        rescue
-          _e in ArgumentError -> handle_error(invoice)
-        end
+      [match] -> 
+        match
+         |> String.replace(["<vNF>", "</vNF>"], "") 
+         |> Decimal.new
     end
   end
   
   def process_sequential() do
     Path.wildcard("invoices/*.xml")
-      |> Enum.reduce(0.0, fn (invoice, acc) -> acc + get_value(invoice) end)
+      |> Enum.reduce(Decimal.new(0), fn (invoice, acc) -> Decimal.add(acc, get_value(invoice)) end)
   end
   
   def process_parallel() do
+  # NEXT:
+  # - remake the concurrency using a supervised process
+  
     Path.wildcard("invoices/*.xml")
       |> Task.async_stream(&get_value/1)
-      |> Enum.reduce(0.0, fn ({:ok, value}, acc) -> value + acc end)
+      |> Enum.reduce(Decimal.new(0), fn ({:ok, value}, acc) -> Decimal.add(value, acc) end)
   end
   
 end
